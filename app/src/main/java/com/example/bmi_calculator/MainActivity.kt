f802e567ed8a54d81fa03d88b3ef0d8b29531d41
hwayeon <hwayeon351@gmail.com>
2022-01-12 21:22:47 +0900

    feat: Update layout and ResultActivity.kt

--- app/src/main/java/com/example/bmi_calculator/MainActivity.kt
+++ app/src/main/java/com/example/bmi_calculator/MainActivity.kt
@@ -26,6 +26,8 @@ class MainActivity : AppCompatActivity() {
             val weight: Int = weightEditText.text.toString().toInt()
 
             val intent = Intent(this, ResultActivity::class.java)
+            intent.putExtra("height", height)
+            intent.putExtra("weight", weight)
             startActivity(intent)
         }
     }
